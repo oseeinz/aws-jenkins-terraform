9329d5d45e4a60399f1eb17ec7056a3b5d9b7833
Ose <54693132+oseeinz@users.noreply.github.com>
2019-11-02 21:43:43 -0400

    Update and rename main.tf to s3_backend.tf

--- terraform/s3_backend.tf
+++ terraform/s3_backend.tf
@@ -6,10 +6,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-# Configure the AWS Provider
-provider "aws" {
-#  access_key = "${var.aws_access_key}"
-#  secret_key = "${var.aws_secret_key}"
-  region  = "${var.aws_region}"
-}
