6fb66be8e70f5b17db61443dd45774a838b2c38f
Ose <54693132+oseeinz@users.noreply.github.com>
2019-11-02 21:41:21 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,18 +13,3 @@ provider "aws" {
 #  secret_key = "${var.aws_secret_key}"
   region  = "${var.aws_region}"
 }
-#Lauch EC2 with the following resources
-#resource "aws_instance" "TF_web" {
-#  #ami                    = "${var.ami}"
-#  ami                    = "${data.aws_ami.webapp_ami.id}"
-#  instance_type          = "${var.instance_type}"
-#  key_name               = "${var.key_name}"
-#  vpc_security_group_ids = ["${aws_security_group.TF_sg.id}"]
-#  subnet_id              = "${aws_subnet.TFsub_pub.id}"
-#  associate_public_ip_address = true
-#  user_data              = "${file("enable_tomcat.sh")}"
-#
-#  tags = {
-#    "Environment" = "${var.environment}"
-#  }
-#}
