06167f589022174ddf1cb1ccbc3e3320a39d94e0
Ose <54693132+oseeinz@users.noreply.github.com>
2019-11-14 01:30:54 -0500

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,17 +1,3 @@
-#Input access key
-variable "aws_access_key" {
-  default = ""
-}
-#Input secret key
-variable "aws_secret_key" {
-  default = ""
-}
-
-#set default region
-variable "aws_region" {
-  default = "us-east-1"
-}
-
 #Custom AMI to be used to launch instance
 #variable "ami" {
 #  default = "ami-0fa09f8066ae75f1a"
